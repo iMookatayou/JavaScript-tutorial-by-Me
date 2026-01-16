3a6f0dce4f6e6faaea21e9f0204a1667334f2912
iMookatayou <jetsribumrungtechin@gmail.com>
2026-01-16 20:05:45 +0700

    lesson 5 array

---
+++ app/lesson5/arraycolors.js
@@ -0,0 +1,10 @@
+const colors = ["red", "blue", "green", "yellow"]
+
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors[3]);
+
+//สร้างตัวแปร colors เก็บอาเรย์สี 4 สี
+//แสดงค่าสีทั้ง 4 สี ออกมาทางคอนโซล โดยการเข้าถึงค่าผ่านดัชนีของอาเรย์
+//ดัชนีเริ่มต้นที่ 0
---
+++ app/lesson5/arraylenght.js
@@ -0,0 +1,7 @@
+const colors = ["red", "green", "blue", "yellow"];
+
+console.log(colors.length);
+
+//สร้างตัวแปร colors เก็บอาเรย์สี 4 สี
+//แสดงความยาวของอาเรย์ colors ออกทางหน้าจอคอนโซล
+//ผลลัพธ์ที่ได้ 4
