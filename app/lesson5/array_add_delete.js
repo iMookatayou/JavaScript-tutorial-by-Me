4606eb9d5b709e674900df6d4195dfc09492673d
iMookatayou <jetsribumrungtechin@gmail.com>
2026-01-21 23:29:07 +0700

    lesson 5 array

---
+++ app/lesson5/array_add_delete.js
@@ -0,0 +1,14 @@
+const fruits = ['apple', 'banana'];
+
+fruits.push('orange'); 
+fruits.unshift('mongo');
+fruits.pop();
+
+console.log(fruits);
+
+//สร้างตัวแปร fruits เก็บอาเรย์ผลไม้ 2 ชนิด
+//เพิ่มผลไม้ 1 ชนิดไปท้ายอาเรย์
+//เพิ่มผลไม้ 1 ชนิดไปหน้าอาเรย์
+//ลบผลไม้ 1 ชนิดจากท้ายอาเรย์
+//แสดงผลลัพธ์ของอาเรย์ fruits ออกทางหน้าจอคอนโซล
+//ผลลัพธ์ที่ได้ ['mongo', 'apple', 'banana']
